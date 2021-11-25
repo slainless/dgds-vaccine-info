5123f35b66f24c4e9324fbd779fadb7a713a8199
Aiman Fauzy <aiman.fauzy.55@gmail.com>
2021-11-25 12:53:29 +0800

    refactor useLocation to emit Promise

--- src/functions/useLocations.ts
+++ src/functions/useLocations.ts
@@ -3,29 +3,35 @@ import type { Cities, City, Locations, Province } from 'types/api'
 import isValidResponse from './isValidResponse'
 import isValidLocations from './isValidLocations'
 
-export function useLocations(input?: {
-  province: Province
-  city: City
-}) {
+export function useLocations(input?: { province: Province; city: City }) {
   const [loading, setLoading] = useState(false)
   const [locations, setLocations] = useState<Locations[]>([])
 
   function start() {
-    if(input == null) return
-    const { province, city } = input
-    setLoading(true)
-    fetch(`https://api.vaksinasi.id/locations/${province}?city=${city}`)
-      .then(res => res.json())
-      .then(res => {
-        if(isValidResponse(res)) {}
-        else throw new Error('Oops! Looks like there are some error on API side!')
+    return new Promise<Locations[]>((resolve, reject) => {
+      if (input == null) return
+      const { province, city } = input
+      setLoading(true)
+      fetch(`https://api.vaksinasi.id/locations/${province}?city=${city}`)
+        .then((res) => res.json())
+        .then((res) => {
+          if (isValidResponse(res)) {
+          } else
+            throw new Error(
+              'Oops! Looks like there are some error on API side!',
+            )
 
-        if(isValidLocations(res.data)) {}
-        else throw new Error('Data of locations received is different from the schema')
+          if (isValidLocations(res.data)) {
+          } else
+            throw new Error(
+              'Data of locations received is different from the schema',
+            )
 
-        setLocations(res.data)
-        setLoading(false)
-      })
+          setLocations(res.data)
+          setLoading(false)
+          resolve(res.data)
+        })
+    })
   }
 
   return { locations, start, loading }
